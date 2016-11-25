93762c11a89ac583a94ef3d34b147d21a59376f9
彭子上 <pengzishang@pzs.local>
2016-11-25 14:43:31 +0800

    Initial Commit

---
+++ blueToothTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  blueToothTest
+//
+//  Created by 彭子上 on 2016/11/25.
+//  Copyright © 2016年 彭子上. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ blueToothTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  blueToothTest
+//
+//  Created by 彭子上 on 2016/11/25.
+//  Copyright © 2016年 彭子上. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
